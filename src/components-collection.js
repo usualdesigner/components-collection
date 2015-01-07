87b938aae1ab5c9dc28a9ff14eb4f87e200fc3f3
usualdesigner <usualdesigner@gmail.com>
2015-01-07 09:58:07 +0200

    basic implementation of the init functionality

--- src/components-collection.js
+++ src/components-collection.js
@@ -9,6 +9,8 @@ var ComponentCollection = (function () {
     'use strict';
 
     var CORE_SELECTOR = 'body',
+        DATA_REQUIRED_ATTRIBUTE = 'data-component-required',
+        DATA_LOADED_ATTRIBUTE = 'data-component-loaded',
 
         config = {
 
@@ -17,7 +19,40 @@ var ComponentCollection = (function () {
         },
 
         componentRequired = [],
-        componentLoaded = [];
+        componentLoaded = [],
+        componentHandlers = {},
+        components = [];
+
+    componentHandlers.test1 = function (element) {
+        element.html(_.now());
+    };
+    componentHandlers.test2 = function (element) {
+        element.html(_.now());
+    };
+    componentHandlers.test3 = function (element) {
+        element.html(_.now());
+    };
+
+    function initialize() {
+
+        var elements = $(CORE_SELECTOR).find('[' + DATA_REQUIRED_ATTRIBUTE + ']');
+        _.forEach(elements, function (element) {
+            var componentArray = $(element).attr(DATA_REQUIRED_ATTRIBUTE).split(' ');
+            _.forEach(componentArray, function (componentName) {
+                if (_.has(componentHandlers, componentName)) {
+                    if (_.isFunction(componentHandlers[componentName])) {
+                        //_.bind(componentHandlers[componentName], $(element));
+                        componentHandlers[componentName]($(element));
+                        console.info("Component has been loaded");
+                    }
+                } else {
+                    console.warn("Can't load component '" + componentName + "'");
+                }
+            });
+        });
+    }
+
+    initialize();
 
     function getComponents() {
 
