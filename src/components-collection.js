b3ddddfe31344d1b9b12a20de378e81e286bf8b1
usualdesigner <usualdesigner@gmail.com>
2015-01-07 08:44:15 +0200

    skeleton

---
+++ src/components-collection.js
@@ -0,0 +1,40 @@
+/*!
+ * component-collection.js
+ * MIT licensed
+ *
+ * Copyright (C) 2015 Aleksey Bernackiy, http://bernackiy.name
+ */
+var ComponentCollection = (function () {
+
+    'use strict';
+
+    var CORE_SELECTOR = 'body',
+
+        config = {
+
+            dependencies: []
+
+        },
+
+        componentRequired = [],
+        componentLoaded = [];
+
+    function getComponents() {
+
+        //
+
+    }
+
+    function setComponents() {
+
+        //
+
+    }
+
+    function loadComponents() {
+
+        //
+
+    }
+
+})();
